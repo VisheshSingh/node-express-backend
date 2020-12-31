97be9404c40e408f5dd0abce1ff8a864723aeeb9
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-31 12:13:03 -0600

    404 page not found

--- app.js
+++ app.js
@@ -11,5 +11,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found!</h1>');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
