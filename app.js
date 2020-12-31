7534484f2e97f018dcd794fdf31ff83a7c095ced
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-31 12:00:00 -0600

    POST requests

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 
 const app = express();
+// body parser
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.use((req, res, next) => {
-  console.log('In the middleware');
-  next(); // Forward the control to the next middleware
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form>'
+  );
 });
-app.use((req, res, next) => {
-  console.log('In another middleware');
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+app.use('/', (req, res, next) => {
   res.send('<h1>Hello from express.js</h1>');
 });
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
