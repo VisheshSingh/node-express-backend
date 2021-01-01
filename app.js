0698b6e2d2bca608b63f2e88e7b22e7e5467e0a8
Vishesh Singh <VisheshSingh16@gmail.com>
2021-01-01 15:56:00 -0600

    404 error controller

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 // ROUTES
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/errorController');
 
 const app = express();
 
@@ -19,9 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.render('404', { docTitle: '404 not found!', path: '' });
-});
+app.use(errorController.get404);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
