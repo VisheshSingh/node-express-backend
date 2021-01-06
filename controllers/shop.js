45743a592f60861a3d69d5b8a1a0b27e8f768596
Vishesh Singh <VisheshSingh16@gmail.com>
2021-01-06 10:59:35 -0600

    reset the cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -126,9 +126,11 @@ exports.getOrders = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -141,6 +143,9 @@ exports.postOrder = (req, res, next) => {
               return product;
             })
           );
+        })
+        .then(() => {
+          return fetchedCart.setProducts(null);
         })
         .then((result) => res.redirect('/orders'))
         .catch((err) => console.log(err));
