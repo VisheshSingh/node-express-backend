a20bfd4eaa2fcc0fbf8595773deba3257ab0c795
Vishesh Singh <VisheshSingh16@gmail.com>
2021-01-05 22:10:59 -0600

    Deleting Related Items & Deleting Cart Products

--- controllers/shop.js
+++ controllers/shop.js
@@ -101,10 +101,18 @@ exports.postCart = (req, res, next) => {
 
 exports.postDeleteCartProduct = (req, res, next) => {
   const productId = req.body.productId;
-  Product.findById(productId, (product) => {
-    Cart.deleteProduct(productId, product.price);
-  });
-  res.redirect('/cart');
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: productId } });
+    })
+    .then((products) => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then(() => res.redirect('/cart'))
+    .catch((err) => console.log(err));
 };
 
 // @desc    Get Orders
